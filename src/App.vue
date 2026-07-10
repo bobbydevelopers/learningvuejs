113d89de06a2a9367026b41cd7fad0baf9a90e0d
Bobby Singh <bobbymarkbs@gmail.com>
2026-07-10 10:07:48 +0530

    added the all basic fiels

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import Signup from './components/Admin/Signup.vue';
+
+</script>
+
+<template>
+
+</Signup>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/Admin/Signup.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+    <div class="form-main-container">
+        <input type="text" placeholder="Enter Email Address"/>
+    </div>
+</template>
+
+<style scoped>
+.form-main-container {
+    margin: 0px auto;
+    width: 80%;
+    border: 1px solid black;
+}
+</style>
